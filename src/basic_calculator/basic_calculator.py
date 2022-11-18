7c76c803b1ebd4ebf99a420890ba0d5c5d832172
Arthur Edmond Jenkins <deesrex@fox5.co.za>
2022-11-18 12:36:57 +0200

    Fixing recursion bug

--- src/basic_calculator/basic_calculator.py
+++ src/basic_calculator/basic_calculator.py
@@ -8,6 +8,7 @@ class BasicCalculator:
             '-': self.subtract
         }
         self.operations = {}
+        self.equation = ""
 
     def add(self, a, b):
         return a + b
@@ -16,26 +17,63 @@ class BasicCalculator:
         return a - b
 
     def multiply(self, a, b):
+        print(a, b)
         return a * b
 
     def divide(self, a, b):
         return a / b
 
     def calculate(self, equation):
-        for i, char in enumerate(equation):
-            if char in self.operators:
-                operator = self.operators[char]
-                self.operations[i] = [char, equation[i-1], equation[i+1], operator]
+        self.equation = equation
+        while self.contains_operation(self.equation):
+            self.equation = self.calculate_equation(self.equation)
 
+        return self.equation
 
-        for i, operation in self.operations.items():
-            result = operation[3](int(operation[1]), int(operation[2]))
-            to_replace = operation[1] + operation[0] + operation[2]
-            equation = equation.replace(to_replace, str(result))
-        
+    def calculate_equation(self, equation):
+        self.get_operations(equation)
+        equation = equation.replace(" ", "")
+        rearranged = self.rearrange_operations()
+        print(rearranged)
+        to_find = rearranged[0]
+        left = to_find.split(to_find[1])[0]
+        right = to_find.split(to_find[1])[1]
+        to_replace_value = self.operators[to_find[1]]
+        equation = equation.replace(to_find, str(to_replace_value(int(left), int(right))))
         return equation
+
+    def rearrange_operations(self):
+        new_stack = []
+        for operation in self.stack:
+            print(operation + " OPERATION")
+            if operation[1] == '*' or operation[1] == '/':
+                new_stack.insert(0, operation)
+            else:
+                new_stack.insert(-1, operation)
+
+        return new_stack
+
+
+
+    def contains_operation(self, equation):
+        for operation in list(equation):
+            if operation in self.operators:
+                return True
+        return False
+
+    def get_operations(self, equation):
+        self.stack = []
+        for i, char in enumerate(equation.split(" ")):
+            if char in self.operators:
+                operation = equation.split(" ")[i - 1] + char + equation.split(" ")[i + 1]
+                self.stack.append(operation)
+                print("STACK: " + str(self.stack))
 
 if __name__ == "__main__":
     calculator = BasicCalculator()
-    result = calculator.calculate("1+2*3")
-    print(result)
+    cal_1 = "1 + 2"
+    cal_2 = "2 + 5 * 10"
+    
+
+    result = calculator.calculate(cal_2)
+    print("Answer = " + result)
